2617cdae51a941d34a47b325b3263c6e20cad88d
Polina <polina_cherkasova@outlook.com>
2025-11-11 03:28:09 +0200

    fix access

--- app/setup.py
+++ app/setup.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:3306/{os.environ['DB_NAME']}"
