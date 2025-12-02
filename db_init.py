bbbffc0c982d0b21d04cdf2a516b268f2ca24ec9
Polina <polina_cherkasova@outlook.com>
2025-12-02 20:10:04 +0200

    add comment for app path

--- db_init.py
+++ db_init.py
@@ -5,6 +5,7 @@ import mysql.connector
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# add proper app dir
 from app.__init__ import create_tables, populate_data, execute_sql_scripts
 from app.setup import Config
 
